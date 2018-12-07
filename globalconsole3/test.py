bea541d3db394b2056f2f0d9ecf12e51b1ef6650
grzegorzcode <34090941+grzegorzcode@users.noreply.github.com>
2018-12-07 11:02:05 +0100

    v3.0.1 preparation1

--- globalconsole3/test.py
+++ globalconsole3/test.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     ghosts = ghosts.GcHosts(gconf, glog)
 
     # ghosts.purgeHosts()
-    # ghosts.importCsvAsHosts('/home/pl55227/Documents/GlobalConsole/GlobalConsole2/hosts/hostsVM.csv')
+    # ghosts.importCsvAsHosts('/home/myuser/Documents/GlobalConsole/GlobalConsole2/hosts/hostsVM.csv')
     # ghosts.updateHost({'host': '10.10.10.10'}, 'myhost1')
     # ghosts.removeHost('myhost1')
     # print(ghosts.searchHostName('myhost2'))
@@ -50,13 +50,13 @@ if __name__ == '__main__':
     # print(ghosts.hosttable.all())
     # print("--")
     # ghosts.getHosts(True, 'port', 'group')
-    # ghosts.exportHostsToCsv('/home/pl55227/Documents/GlobalConsole/GlobalConsole2/hosts/hosts3exp.csv')
+    # ghosts.exportHostsToCsv('/home/myuser/Documents/GlobalConsole/GlobalConsole2/hosts/hosts3exp.csv')
 
     #creds
     gcreds = gcreds.GcCreds(gconf, glog)
 
     # gcreds.purgeCreds()
-    # gcreds.importCsvAsCreds('/home/pl55227/Documents/GlobalConsole/GlobalConsole2/creds/credsVM.csv')
+    # gcreds.importCsvAsCreds('/home/myuser/Documents/GlobalConsole/GlobalConsole2/creds/credsVM.csv')
     # gcreds.updateCred({'key_password': 'adawddwa'}, 'rootVM')
     # gcreds.removeCred('meVM')
     # print("--")
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     # gcreds.encryptPasswds(_utest=True)
     # gcreds.decryptPasswdsInFile(_utest=True)
     # gcreds.getCreds(False, True, 'credname')
-    # gcreds.exportCredsToCsv('/home/pl55227/Documents/GlobalConsole/GlobalConsole2/creds/creds3exp.csv')
+    # gcreds.exportCredsToCsv('/home/myuser/Documents/GlobalConsole/GlobalConsole2/creds/creds3exp.csv')
 
     #vars
     gvars = gvars.GcVars(gconf, glog)
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     # gcom.scan()
     # gcom.os("cat /etc/shadow | wc -l", sudo=False)
     # gcom.db2("select * from sysibm.sysdummy1", user="instance")
-    # gcom.scp(mode="put", source="/home/pl55227/Documents/GlobalConsole/GlobalConsole2/upload/myfile1.txt", dest="/tmp", recursive=False, suffix=True, batch=True)
+    # gcom.scp(mode="put", source="/home/myuser/Documents/GlobalConsole/GlobalConsole2/upload/myfile1.txt", dest="/tmp", recursive=False, suffix=True, batch=True)
     # gcom.os("ls -la  /tmp/myfile1.txt | awk '{print $1 }'", sudo=True)
     # gcom.os("ls -la  /tmp/myfile1.txt", sudo=False)
     # gcom.close()
