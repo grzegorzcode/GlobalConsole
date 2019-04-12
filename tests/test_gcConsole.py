8c841ee0ebf4756c60463a393ff9b34e30f5fc3a
grzegorzcode <34090941+grzegorzcode@users.noreply.github.com>
2019-04-12 15:32:57 +0200

    panaceum - travis tests

--- tests/test_gcConsole.py
+++ tests/test_gcConsole.py
@@ -68,10 +68,12 @@ class TestGcConsole(TestCase):
         self.gcreds.credfile.close()
         self.ghosts.hostfile.close()
         #DEBUGGING PURPOSES
-        # print("---------------LOG INFO---------------")
-        # with open('logs/global_console.log', 'r') as file:
-        #     for line in file.readlines():
-        #         print(line)
+        print("---------------LOG INFO---------------")
+        import os
+        print(os.getcwd())
+        with open('logs/global_console.log', 'r') as file:
+            for line in file.readlines():
+                print(line)
 
     def test_do_conn(self):
         self.assert_stdout("conn connect -Y", self.gcon.onecmd, 2, "INFO", "ERROR", "connection test")
@@ -90,7 +92,7 @@ class TestGcConsole(TestCase):
         self.assert_stdout("run db2 db2select +OS +USR instance +repeat 2 2", self.gcon.onecmd, 18, "IBMREQD", "ERROR", "db2 command with repetition test")
         self.assert_stdout("run db2 uname -a | sed 's%Linux\s*\(\w*\)%4\t\1%g' | awk '{print $1}' +OS +USR instance", self.gcon.onecmd, 5, "4", "ERROR", "db2 os sed regex test")
         self.assert_stdout("run scp -m put -s /home/travis/.ssh/id_rsa -d /home/travis/rsa -b", self.gcon.onecmd, 4, "id_rsa", "ERROR", "db2 scp test")
-        self.assert_stdout("yaml run -s test.yaml -S", self.gcon.onecmd, 0, "passed", "ERROR", "yaml functionality test")
+        self.assert_stdout("yaml run -s tests/test.yaml -S", self.gcon.onecmd, 0, "passed", "ERROR", "yaml functionality test")
         try:
             with open('/home/travis/rsa', 'r') as file:
                 file.readlines()
